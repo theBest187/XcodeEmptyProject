40f71f1672389cec4716526947e37e23f45b2a4c
KimTae jun <korean.darren@gmail.com>
2015-09-24 08:55:31 +0900

    add empty

---
+++ Playwings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Playwings
+//
+//  Created by KimTae jun on 2015. 9. 24..
+//  Copyright (c) 2015년 Fridaynoons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Playwings/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Playwings
+//
+//  Created by KimTae jun on 2015. 9. 24..
+//  Copyright (c) 2015년 Fridaynoons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
